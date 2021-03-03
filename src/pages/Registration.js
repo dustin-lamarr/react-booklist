6590cca141b86173ddb2eae71c1466dcf59f4335
Alex Puhl <alexpuhldeveloper@gmail.com>
2021-03-02 23:22:04 -0700

    Added default roleId of 1 when creating new users which is tired to basic users in the roles table, also updated email input to have type of email instead of text

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -27,7 +27,7 @@ export default function RegistrationPage() {
           <label htmlFor="email">
             Email
             <input
-              type="text"
+              type="email"
               name="email"
               id="email"
               value={values.email}
